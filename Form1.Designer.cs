b74b71ab42c6039c19bf54a0632a159e3b424043
성현 이 <leetjdgus@gmail.com>
2026-03-19 12:20:21 +0900

    과제1 코딩과 테스트 완료
    ListBox를 중앙에 TextBox 아래에 배치함
    전송 버튼을 누르면 문장이 입력되고 남은 내용은 지워짐

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            listbox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(90, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(200, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Echo Messenger";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("맑은 고딕", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            textBox1.Location = new Point(90, 340);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(482, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // listbox1
+            // 
+            listbox1.FormattingEnabled = true;
+            listbox1.Location = new Point(90, 100);
+            listbox1.Name = "listbox1";
+            listbox1.Size = new Size(622, 199);
+            listbox1.TabIndex = 2;
+            listbox1.SelectedIndexChanged += listbox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(595, 315);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 78);
+            button1.TabIndex = 3;
+            button1.Text = "전송";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listbox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private ListBox listbox1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace EchoMessenger
 {
     public partial class Form1 : Form
@@ -5,6 +9,25 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
+        }
+
+        
+
+        private void listbox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var text = textBox1.Text.Trim();
+            if (!string.IsNullOrEmpty(text))
+            {
+                listbox1.Items.Add(text);
+                textBox1.Clear();
+
+            }
         }
     }
 }
